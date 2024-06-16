f5503d8cf8ecda6085b7242b7ba6712fa6f07045
Marcus <6180175+MKindberg@users.noreply.github.com>
2024-06-16 14:08:07 +0200

    Let lsp lib handle document

--- src/analysis.zig
+++ src/analysis.zig
@@ -1,49 +1,11 @@
 const std = @import("std");
 const lsp_types = @import("lsp").types;
-const Document = @import("lsp").document.Document;
+const Document = @import("lsp").Document;
 const Regex = @import("regex.zig").Regex;
 
 pub const State = struct {
-    allocator: std.mem.Allocator,
-    documents: std.StringHashMap(DocData),
-
-    pub fn init(allocator: std.mem.Allocator) State {
-        return State{
-            .allocator = allocator,
-            .documents = std.StringHashMap(DocData).init(allocator),
-        };
-    }
-    pub fn deinit(self: *State) void {
-        var it = self.documents.iterator();
-        while (it.next()) |i| {
-            self.allocator.free(i.key_ptr.*);
-            i.value_ptr.deinit();
-        }
-        self.documents.deinit();
-    }
-
-    pub fn openDocument(self: *State, name: []const u8, content: []const u8) !void {
-        const key = try self.allocator.alloc(u8, name.len);
-        std.mem.copyForwards(u8, key, name);
-        const doc = try DocData.init(self.allocator, content);
-
-        try self.documents.put(key, doc);
-    }
-
-    pub fn closeDocument(self: *State, name: []const u8) void {
-        const entry = self.documents.fetchRemove(name);
-        self.allocator.free(entry.?.key);
-        entry.?.value.deinit();
-    }
-
-    pub fn updateDocument(self: *State, name: []const u8, text: []const u8, range: lsp_types.Range) !void {
-        var doc = self.documents.getPtr(name).?;
-        try doc.doc.update(text, range);
-    }
-
-    pub fn hover(self: *State, allocator: std.mem.Allocator, id: i32, uri: []u8, pos: lsp_types.Position) ?lsp_types.Response.Hover {
-        const doc = self.documents.get(uri).?;
-        const line = doc.doc.getLine(pos).?;
+    pub fn hover(allocator: std.mem.Allocator, id: i32, doc: Document, pos: lsp_types.Position) ?lsp_types.Response.Hover {
+        const line = doc.getLine(pos).?;
         const char = pos.character;
         const in_str = std.mem.count(u8, line[0..char], "\"") % 2 == 1 and
             std.mem.count(u8, line[char..], "\"") > 0;
@@ -60,25 +22,5 @@ pub const State = struct {
             const res = allocator.dupe(u8, buf.items) catch return null;
             return lsp_types.Response.Hover.init(id, res);
         } else return null;
-    }
-    pub fn free(self: *State, buf: []const u8) void {
-        self.allocator.free(buf);
-    }
-};
-
-const DocData = struct {
-    doc: Document,
-
-    const Self = @This();
-    fn init(allocator: std.mem.Allocator, content: []const u8) !Self {
-        const doc = try Document.init(allocator, content);
-
-        const self = Self{ .doc = doc };
-
-        return self;
-    }
-
-    fn deinit(self: DocData) void {
-        self.doc.deinit();
     }
 };
--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
+const State = @import("analysis.zig").State;
 const lsp = @import("lsp");
 const lsp_types = @import("lsp").types;
-const State = @import("analysis.zig").State;
 
 const Logger = @import("logger.zig").Logger;
 
@@ -10,12 +10,6 @@ const builtin = @import("builtin");
 pub const std_options = .{
     .log_level = if (builtin.mode == .Debug) .debug else .info,
     .logFn = Logger.log,
-};
-
-pub const RunState = enum {
-    Run,
-    ShutdownOk,
-    ShutdownErr,
 };
 
 pub fn main() !u8 {
@@ -30,8 +24,7 @@ pub fn main() !u8 {
     try Logger.init(log_path);
     defer Logger.deinit();
 
-    var state = State.init(allocator);
-    defer state.deinit();
+    var state = State{};
 
     const server_data = lsp_types.ServerData{
         .capabilities = .{
@@ -44,36 +37,15 @@ pub fn main() !u8 {
         },
     };
     var server = lsp.Lsp(State).init(allocator, server_data, &state);
+    defer server.deinit();
 
-    server.registerDocOpenCallback(handleOpenDoc);
-    server.registerDocChangeCallback(handleChangeDoc);
-    server.registerDocCloseCallback(handleCloseDoc);
     server.registerHoverCallback(handleHover);
 
     return server.start();
 }
 
-fn handleOpenDoc(_: std.mem.Allocator, state: *State, params: lsp_types.Notification.DidOpenTextDocument.Params) void {
-    const doc = params.textDocument;
-    std.log.info("Opened {s}", .{doc.uri});
-    std.log.debug("{s}", .{doc.text});
-    state.openDocument(doc.uri, doc.text) catch unreachable;
-}
-
-fn handleChangeDoc(_: std.mem.Allocator, state: *State, params: lsp_types.Notification.DidChangeTextDocument.Params) void {
-    for (params.contentChanges) |change| {
-        state.updateDocument(params.textDocument.uri, change.text, change.range) catch unreachable;
-    }
-
-    std.log.debug("Updated document {s}", .{state.documents.get(params.textDocument.uri).?.doc.text});
-}
-
-fn handleCloseDoc(_: std.mem.Allocator, state: *State, params: lsp_types.Notification.DidCloseTextDocument.Params) void {
-    state.closeDocument(params.textDocument.uri);
-}
-
-fn handleHover(allocator: std.mem.Allocator, state: *State, request: lsp_types.Request.Hover.Params, id: i32) void {
-    if (state.hover(allocator,id, request.textDocument.uri, request.position)) |response| {
+fn handleHover(allocator: std.mem.Allocator, context: lsp.Lsp(State).Context, request: lsp_types.Request.Hover.Params, id: i32) void {
+    if (State.hover(allocator, id, context.document, request.position)) |response| {
         defer allocator.free(response.result.contents);
         lsp.writeResponse(allocator, response) catch unreachable;
 
